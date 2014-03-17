bfc3b317fb96bae6767bf58c548f1d1880dabfac
Ricky Dunlop <hello@rickydunlop.co.uk>
2014-03-17 17:50:10 +0000

    Fixed strict warnings in I18nable behavior

--- Model/Behavior/I18nableBehavior.php
+++ Model/Behavior/I18nableBehavior.php
@@ -12,8 +12,8 @@
 /**
  * i18nable Behavior
  *
- * @package		i18n
- * @subpackage	i18n.models.behaviors
+ * @package     i18n
+ * @subpackage  i18n.models.behaviors
  */
 class I18nableBehavior extends ModelBehavior {
 
@@ -22,71 +22,71 @@ class I18nableBehavior extends ModelBehavior {
  *
  * @var array
  */
-	public $defaults = array(
-		'languageField' => 'language_id'
-	);
+    public $defaults = array(
+        'languageField' => 'language_id'
+    );
 
 /**
  * Settings array
  *
  * @var array
  */
-	public $settings = array();
+    public $settings = array();
 
 /**
  * Setup
  *
- * @param AppModel $Model
+ * @param AppModel $model
  * @param array $settings
  */
-	public function setup($Model, $settings = array()) {
-		if (!isset($this->settings[$Model->alias])) {
-			$this->settings[$Model->alias] = $this->defaults;
-		}
-		$this->settings[$Model->alias] = array_merge($this->settings[$Model->alias], is_array($settings) ? $settings : array());
-	}
+    public function setup(Model $model, $config = array()) {
+        if (!isset($this->settings[$model->alias])) {
+            $this->settings[$model->alias] = $this->defaults;
+        }
+        $this->settings[$model->alias] = array_merge($this->settings[$model->alias], is_array($config) ? $config : array());
+    }
 
 /**
  * Add language filter
  *
- * @param Model $Model
+ * @param Model $model
  * @param array $query
  * @return array|bool
  */
-	public function beforeFind(Model $Model, $query) {
-		if (empty($this->settings[$Model->alias])) {
-			return;
-		}
-		$settings = $this->settings[$Model->alias];
-		$language = Configure::read('Config.language');
-		if ($Model->hasField($settings['languageField']) && (!isset($query['ignoreLanguage']))) {
-			if (empty($query['conditions'][$Model->alias . '.' . $settings['languageField']])) {
-				if (isset($query['language'])) {
-					$language = $query['language'];
-				}
-				$query['conditions'][$Model->alias . '.' . $settings['languageField']] = $language;
-			}
-		}
-		return $query;
-	}
+    public function beforeFind(Model $model, $query) {
+        if (empty($this->settings[$model->alias])) {
+            return;
+        }
+        $settings = $this->settings[$model->alias];
+        $language = Configure::read('Config.language');
+        if ($model->hasField($settings['languageField']) && (!isset($query['ignoreLanguage']))) {
+            if (empty($query['conditions'][$model->alias . '.' . $settings['languageField']])) {
+                if (isset($query['language'])) {
+                    $language = $query['language'];
+                }
+                $query['conditions'][$model->alias . '.' . $settings['languageField']] = $language;
+            }
+        }
+        return $query;
+    }
 
 /**
- * Set current language 
+ * Set current language
  *
- * @param Model $Model
+ * @param Model $model
  * @return void
  */
-	public function beforeSave(Model $Model) {
-		if (empty($this->settings[$Model->alias])) {
-			return;
-		}
-		$settings = $this->settings[$Model->alias];
-		$language = Configure::read('Config.language');
-		if ($Model->hasField($settings['languageField'])) {
-			if (empty($Model->data[$Model->alias][$settings['languageField']])) {
-				$Model->set(array($settings['languageField'] => $language));
-			}
-		}
-		return true;
-	}
+    public function beforeSave(Model $model, $options = array()) {
+        if (empty($this->settings[$model->alias])) {
+            return;
+        }
+        $settings = $this->settings[$model->alias];
+        $language = Configure::read('Config.language');
+        if ($model->hasField($settings['languageField'])) {
+            if (empty($model->data[$model->alias][$settings['languageField']])) {
+                $model->set(array($settings['languageField'] => $language));
+            }
+        }
+        return true;
+    }
 }
